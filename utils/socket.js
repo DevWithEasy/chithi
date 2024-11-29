ff48d7fea2b6b66ad762a84b3d461c7ae9628b10
DevWithEasy <devwitheasy@gmail.com>
2024-11-29 13:51:26 +0600

    socket error

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.NODE_ENV === 'production' ? 'https://chithibox-api.vercel.app' : 'http://localhost:8080')
+const socket = io(process.env.NODE_ENV === 'production' ? 'https://chithibox-api.vercel.app/' : 'http://localhost:8080/')
 
 export default socket
